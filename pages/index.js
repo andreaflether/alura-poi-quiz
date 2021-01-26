deebbecdad8bad993648518a904e4ec012c0465e
Andréa Alencar <andreaflether@gmail.com>
2021-01-26 02:28:30 -0300

    Código ao final da primeira aula - Imersão React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GithubCorner from '../src/components/GithubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Person of Interest</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Descrição do quiz...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+          <h1>Quizes da galera</h1>
+          <p>Descrição do quiz...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="http://github.com/andreaflether"/>
+    </QuizBackground>
+  )
 }
